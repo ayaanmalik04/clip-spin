8816aa801008dea3bfadb0babf004125ac607103
ayaanmalik04 <131360653+ayaanmalik04@users.noreply.github.com>
2024-05-30 23:58:57 -0700

    Add files via upload

---
+++ model/models.py
@@ -0,0 +1,6 @@
+# from linknet import LinkNet34, LinkNet34MTL
+from .stack_module import StackHourglassNetMTL_DGCNv4
+# from SPIN import spin
+# from unet import unet
+
+MODELS = {"model": StackHourglassNetMTL_DGCNv4}
